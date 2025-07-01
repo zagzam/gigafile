0d76f5d049c0cf09b3d7f22ee0a9a06f4573aec8
fireattack <human.peng@gmail.com>
2025-07-02 03:03:03 +0800

    Bump version

--- gfile/__init__.py
+++ gfile/__init__.py
@@ -1,4 +1,4 @@
 from .gfile import GFile
 
 __author__ = """Sraqzit, fireattack"""
-__version__ = '3.2.3'
+__version__ = '3.2.4'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gigafile',
-    version='3.2.3',
+    version='3.2.4',
     description='A python module to download and upload from gigafile.nu',
     author='Sraqzit, fireattack',
     install_requires=['requests>=2.25.1', 'requests_toolbelt>=0.9.1', 'tqdm>=4.61.2', 'beautifulsoup4>=4'],
